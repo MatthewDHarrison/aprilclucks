e154e1eaf83bdac233068a76ea0f8af5a96b5df0
MatthewDHarrison <madoha5812@gmail.com>
2024-08-11 16:15:56 +1000

    chore: intialise react.ga

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { theme } from './Theme'
 import { ThemeProvider } from '@emotion/react'
 import './App.css'
 import { CssBaseline } from '@mui/material'
+import ReactGA from 'react-ga'
+
+ReactGA.initialize("GTM-KDTKH5CL");
 
 function App() {
   return (
